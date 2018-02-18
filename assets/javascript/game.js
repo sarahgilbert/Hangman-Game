216f2d26978bf02cc2619d4213b041acdf0fd475
Sarah Gilbert <sarahjgilbert1@yahoo.com>
2018-02-17 19:52:32 -0700

    second commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,31 @@
 
-// create a bank of possible words
+// create a bank of possible words so that the computer can choose a random word
 var words = ["adventure", "treasure", "galleon", "plank", "swashbuckling"];
 
 // make a way for the computer to randomly choose one of the words
-var words = words[Math.floor(Math.random() * words.length)];
+var thisWord = words[Math.floor(Math.random() * words.length)];
 
 // make an array filled with _ to match the number of letters in each word
-
+// before the player starts guessing, show the player little lines representing each of the letters in the word
 var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
-        answerArray[i] = "_";
+    for (var i = 0; i < thisWord.length; i++) {
+       answerArray[i] = "_";
     }
-
+// ?loop -for, while, do?
+// get a guess from the player
 // connect the guessing form in html to javascript
+//document.getElementById("guess");
 
+// if the guess is in the word, update the player's progress by showing the letter on the right line
+
+//if the letter guessed isn't in the word, update the number of lives remaining
 //keep track of letters left to be guessed
-var remainingLetters = word.length;
+//var remainingLetters = word.length;
 
 // show which letters have been guessed 
 
-//put the letters on the right lines so that the player can see
-
-//limit the number of wrong guesses a user can make
-
-// end the game if the user wins, or makes too many wrong guesses
-
-// display the number of wins
+//if the number of lives remaining is zero, end the game
+// if the player guesses the word, update the number of wins
 
 
 
@@ -33,23 +33,23 @@ var remainingLetters = word.length;
 // stuff below this is from rock, paper, scissors activity
 
 // create a variable to hold the number of wins //
-var wins = 0;
+//var wins = 0;
 
 
 // create a function to run whenever the user presses a key
-document.onkeypress =function(event) {
+//document.onkeypress =function(event) {
 // determines which key was pressed
-    var userGuess = event.key;
-}
+  //  var userGuess = event.key;
+//}
 
  // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
  var computerChoices = ["r", "p", "s"];
 
  // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
 //var html =
- "<p>You chose: " + userGuess + "</p>" +
+// "<p>You chose: " + userGuess + "</p>" +
  
- "<p>wins: " + wins + "</p>";
+ //"<p>wins: " + wins + "</p>";
  
 // Set the inner HTML contents of the #game div to our html string
 //document.querySelector("#game").innerHTML = html;
